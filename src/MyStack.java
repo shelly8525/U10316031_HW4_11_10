23e66ee67247ac6ac2673d18f828364aa27208b1
shelly8525 <shelly8525@yahoo.com.tw>
2015-04-17 00:42:54 +0800

    建一個MyStack繼承ArrayList
    設isEmpty()方法回傳

---
+++ src/MyStack.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+public class MyStack<s> extends ArrayList<s> {
+	public Mystack(){
+		super();
+	}
+	
+	public boolean isEmpty(){
+		if(this.isEmpty()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	publicint getSize(){
+		return 
+	}
+}
