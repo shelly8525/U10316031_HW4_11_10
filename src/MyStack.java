9aecdf015930932fccb0db77b8d50ca097012074
shelly8525 <shelly8525@yahoo.com.tw>
2015-04-17 21:15:07 +0800

    註解

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,24 +1,25 @@
 import java.util.ArrayList;
 
 class MyStack extends ArrayList<String> {
+	//create a boolean method
 	public boolean isEmpty(){
 		return super.isEmpty();
 	}
-	
+	//create a getSize method
 	public int getSize(){
 		return super.size();
 	}
-	
+	//create a peek method
 	 public String peek() {
 		 return super.get(getSize() - 1);
 	 }
-	 
+	 //create a pop method
 	 public String pop() {
 		 String o = super.get(getSize() - 1);
 		 super.remove(getSize() - 1);
 		 return o;
 	 }
-	 
+	 //create a push method
 	 public void push(String o){
 		super.add(o); 
 	 }
--- src/TestMyStack.java
+++ src/TestMyStack.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 public class TestMyStack {
+	//main method
 	public static void main(String[] args){
-		MyStack s = new MyStack();
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter five strings:");
-		
+		MyStack s = new MyStack();//new an object
+		Scanner input = new Scanner(System.in);//create a Scanner
+		System.out.print("Enter five strings:");//print five strings
+		//create a forloop
 		for(int i = 0;i <= 4;i++){
 			s.push(input.next());
 		}
-		
+		//create a forloop
 		for(int a = s.getSize();a >= 1;a--){
 			System.out.print(s.pop());
 		}
